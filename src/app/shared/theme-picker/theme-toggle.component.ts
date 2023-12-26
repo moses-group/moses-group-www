285479a0057e468d9efd583d446ad64ccefb78f2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-12-25 23:02:30 -0500

    [{src/assets/styles,index.html,src/app/shared/theme-picker/theme-toggle.component.ts}] Upgrade to latest from angular/angular aio

--- src/app/shared/theme-picker/theme-toggle.component.ts
+++ src/app/shared/theme-picker/theme-toggle.component.ts
@@ -1,6 +1,5 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-
 import { LocalStorage } from '../storage.service';
 
 export const storageKey = 'aio-theme';
